99b7f4ca3f60152279d997d76d7edcf1593867da
Trung Lê <joneslee85@gmail.com>
2011-08-18 13:09:24 +1000

    Fix missing closing bracket

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -2,4 +2,5 @@ Image.class_eval do
   attachment_definitions[:attachment] = (attachment_definitions[:attachment] || {}).merge({
     :storage => Rails.env.production? ? 's3' : 'filesystem',
     :s3_credentials => Rails.root.join('config', 's3.yml')
+  })
 end
