63f0cfa47089cae50365e226ec7dd1eccde0ac05
Trung Lê <joneslee85@gmail.com>
2012-02-27 00:20:28 +1100

    Allow to use Heroku config var

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,6 +1,23 @@
 Spree::Image.class_eval do
-  attachment_definitions[:attachment] = (attachment_definitions[:attachment] || {}).merge({
-    :storage => Rails.env.production? ? 's3' : 'filesystem',
-    :s3_credentials => Rails.root.join('config', 's3.yml')
-  })
+  if Rails.env.production?
+    if ENV['S3_KEY'] && ENV['S3_SECRET'] && ENV['S3_BUCKET']
+      S3_OPTIONS = {
+        :storage => 's3',
+        :s3_credentials => {
+          :access_key_id     => ENV['S3_KEY'],
+          :secret_access_key => ENV['S3_SECRET']
+        },
+        :bucket => ENV['S3_BUCKET']
+      }
+    else
+      S3_OPTIONS = {
+        :storage => 's3',
+        :s3_credentials => Rails.root.join('config', 's3.yml')
+      }
+    end
+  else
+    S3_OPTIONS = { :storage => 'filesystem' }
+  end
+
+  attachment_definitions[:attachment] = (attachment_definitions[:attachment] || {}).merge(S3_OPTIONS)
 end
