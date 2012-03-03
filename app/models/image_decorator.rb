3aa35c1229e3d5efa8966e54d8d5355fea9edaa7
Ben Wiseley <wiseleyb@gmail.com>
2012-03-04 10:13:11 +1100

    Removing production only check - should work on all environments. Forcing config/s3.yml.

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,23 +1,23 @@
 Spree::Image.class_eval do
-  if Rails.env.production?
-    if ENV['S3_KEY'] && ENV['S3_SECRET'] && ENV['S3_BUCKET']
-      S3_OPTIONS = {
-        :storage => 's3',
-        :s3_credentials => {
-          :access_key_id     => ENV['S3_KEY'],
-          :secret_access_key => ENV['S3_SECRET']
-        },
-        :bucket => ENV['S3_BUCKET']
-      }
-    else
+  # if Rails.env.production?
+  #   if ENV['S3_KEY'] && ENV['S3_SECRET'] && ENV['S3_BUCKET']
+  #     S3_OPTIONS = {
+  #       :storage => 's3',
+  #       :s3_credentials => {
+  #         :access_key_id     => ENV['S3_KEY'],
+  #         :secret_access_key => ENV['S3_SECRET']
+  #       },
+  #       :bucket => ENV['S3_BUCKET']
+  #     }
+  #   else
       S3_OPTIONS = {
         :storage => 's3',
         :s3_credentials => Rails.root.join('config', 's3.yml')
       }
-    end
-  else
-    S3_OPTIONS = { :storage => 'filesystem' }
-  end
+  #   end
+  # else
+  #   S3_OPTIONS = { :storage => 'filesystem' }
+  # end
 
   attachment_definitions[:attachment] = (attachment_definitions[:attachment] || {}).merge(S3_OPTIONS)
 end
